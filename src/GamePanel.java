fd9b5076d9ea97a0618abeecea0b0affb396f402
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-03-30 14:54:43 +0300

    Define base game configurations in GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,15 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
+    //screen dimensions
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT=600;
+    //item sizes
+    static final int UNIT_SIZE = 25;
+    //the total number of game units
+    static final int GAME_UNITS =(SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    //delay parameter will be used for controlling game speed or animations
+    static final int DELAY= 75;
 
     GamePanel(){
         //constructor
