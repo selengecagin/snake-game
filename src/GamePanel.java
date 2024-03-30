6a2b6ac427640569b549308bbe40391c5a0255da
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-03-30 20:34:44 +0300

    Implement random color generation for UI element

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -67,6 +67,7 @@ public class GamePanel extends JPanel implements ActionListener {
                     g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
                 }else {
                     g.setColor(new Color(45,180,0));
+                    g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
                     g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
                 }
             }
