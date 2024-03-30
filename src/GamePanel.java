304a0260abda9fb75e98543c267bda3c573b9ec6
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-03-30 19:22:55 +0300

    Add rendering for snake head and body in draw method

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -62,8 +62,19 @@ public class GamePanel extends JPanel implements ActionListener {
             g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
         }
 
-    g.setColor(Color.red);
-    g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+
+        for(int i=0; i<bodyParts;i++){
+            if(i==0){
+                g.setColor(Color.green);
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            }else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            }
+        }
     }
 
     public void newApple(){
@@ -72,6 +83,26 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void move(){
+        //itarete through all body parts of the snake
+        for(int i = bodyParts;i>0;i--){
+            x[i]=x[i-1];
+            y[i]=y[i-1];
+        }
+
+        switch(direction){
+            case 'U':
+                y[0]=y[0]-UNIT_SIZE;
+                break;
+            case 'D':
+                y[0]=y[0]+UNIT_SIZE;
+                break;
+            case 'L':
+                x[0]=x[0]-UNIT_SIZE;
+                break;
+            case 'R':
+                x[0]=x[0]+UNIT_SIZE;
+                break;
+        }
 
     }
 
